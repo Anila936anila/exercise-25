a72584f75d98d566c4b2fb11e5497597029352e9
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:55:42 -0800

    Alien Color
    Typescript 45Q assignment exercise 25

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+let alien_color = 'green';
+if (alien_color == 'green') {
+    console.log("Congratulation! You just earned 5 points");
+}
+// second version
+alien_color = 'blue';
+if (alien_color == 'green') {
+    console.log("congratulation! You just earned 5 points.");
+}
